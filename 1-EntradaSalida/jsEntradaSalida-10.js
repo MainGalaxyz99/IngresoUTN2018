25189f061ba3c573d0db26f59dbabc08b701e6ff
MainGalaxyz99 <36174906+MainGalaxyz99@users.noreply.github.com>
2018-02-15 16:45:01 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,14 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
-	
+    var importe;
+    var descuento;
+    var importeFinal;
+
+    importe=parseInt(document.getElementById("importe").value);
+    descuento=importe * .25;
+    importeFinal=(importe - descuento);
+
+    document.getElementById("resultado").value=importeFinal;
+
 }
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,19 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad=document.getElementById("edad").value;
 
+if(edad>=18)
 
+alert("Es mayor de edad");
+
+else if(edad>=13 && edad <=17)
+
+alert("Es adolescente");
+
+else
+
+alert("Es menor de edad");
 }//FIN DE LA FUNCIÓN
