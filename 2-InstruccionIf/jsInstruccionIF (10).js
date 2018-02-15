cb4e83c5fa5690ce46a04704097be04a4370f15f
MainGalaxyz99 <36174906+MainGalaxyz99@users.noreply.github.com>
2018-02-15 20:51:31 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,20 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota=Math.floor(Math.random()*10)+1;
+	 
+	if(nota>=9)
+{
+	alert( nota + " Excelente");
 
-}//FIN DE LA FUNCIÓN
+}	
+	else if(nota>=4 && nota<9)
+{
+	alert( nota + " Aprobó");
+
+}	
+	else
+
+	alert( nota + " Vamos, para la proxima se puede")
+}
+//FIN DE LA FUNCIÓN
