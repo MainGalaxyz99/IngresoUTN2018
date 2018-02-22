90d6a9887745b2824487495dbf1e1b24f8beae52
MainGalaxyz99 <36174906+MainGalaxyz99@users.noreply.github.com>
2018-02-22 16:29:06 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,8 +2,8 @@ function Mostrar()
 {
 
 	var contador=0;
-	var max;
-	var min;
+	var max=0;
+	var min=0;
 	var numero;
 	var respuesta;
 	// declarar variables
@@ -11,25 +11,24 @@ function Mostrar()
 		numero=parseInt(prompt("Ingrese un numero"));
 	if(contador == 1)
 	{
-	min=numero
-	max=numero
+	min=numero;
+	max=numero;
 	}
-	if(numero < min)
+	if(numero > max)
 	{
-	min=numero	
+	max = numero;	
 	}
-    if
-	
-	
-	
-
-	
-	
-		
-	
+    if(numero < min )
+    {
+	min = numero;	
 	}
+	
+	respuesta = prompt("¿Desea continuar? si/no");
+	
+	}while(respuesta == "si")
 
-
+	document.getElementById("maximo").value = max;
+	document.getElementById("minimo").value = min;
 
 
 }//FIN DE LA FUNCIÓN
